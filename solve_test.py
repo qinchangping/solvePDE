39b0134b4fa3b6832a2aac30a4e872ae8510373a
qinchangping <qcp08@163.com>
2018-05-28 17:41:51 +0800

    Update solve_test.py

--- solve_test.py
+++ solve_test.py
@@ -22,7 +22,7 @@ objective function: f = u_t+u*u_x-(0.01/pi)*u_xx
 
 LEARNING_RATE_BASE = 0.0001  # 学习率
 LEARNING_RATE_DECAY = 0.99  # 学习率的衰减率
-REGULARIZATION_RATE = 0.001  # 描述模型复杂度的正则化项在损失函数中的系数
+REGULARIZATION_RATE = 0.0001  # 描述模型复杂度的正则化项在损失函数中的系数
 TRAINING_STEPS = 5000  # 训练轮数
 MOVING_AVERAGE_DECAY = 0.99  # 滑动平均衰减率
 
@@ -40,10 +40,10 @@ class NN:
         # 数据集相关参数
         self.INPUT_NODE = 2  # 输入层节点数，[x,t]
         self.OUTPUT_NODE = 1  # 输出层节点数，u
-        self.BATCH_SIZE = 10000  # 一个训练batch中的训练数据个数
+        self.BATCH_SIZE = 1000  # 一个训练batch中的训练数据个数
 
         # 神经网络相关参数
-        self.LAYER_NODE = [50, 50, 50, 50]  # 隐藏层节点数。5层，5*50个节点
+        self.LAYER_NODE = [50, 50, 50, 50, 50, 50, 50, 50]  # 隐藏层节点数。
         self.NUM_LAYER = len(self.LAYER_NODE)  # 隐藏层数
         # 生成隐藏层的参数
         self.weights = []
@@ -72,12 +72,6 @@ class NN:
 
     def inference(self, input_tensor):
         '''辅助函数，给定网络的输入和所有参数，计算神经网络的前向传播结果。'''
-        # layer1 = tf.nn.tanh(tf.matmul(input_tensor, self.weights1) + self.biases1)
-        # layer2 = tf.nn.tanh(tf.matmul(layer1, self.weights2) + self.biases2)
-        # layer1 = tf.nn.relu(tf.matmul(input_tensor, self.weights1) + self.biases1)
-        # layer2 = tf.nn.relu(tf.matmul(layer1, self.weights2) + self.biases2)
-        # layer1 = tf.nn.sigmoid(tf.matmul(input_tensor, self.weights1) + self.biases1)
-        # layer2 = tf.nn.sigmoid(tf.matmul(layer1, self.weights2) + self.biases2)
         # 计算输出层前向传播结果。
         layer = []
         layer.append(tf.nn.relu(tf.matmul(input_tensor, self.weights[0]) + self.biases[0]))
@@ -111,15 +105,15 @@ class NN:
         # 生成训练点
         x = np.random.uniform(-1, 1, [self.BATCH_SIZE])
         index = np.random.randint(0, self.BATCH_SIZE, [4])
-        #x[index[0]] = -1
-        #x[index[1]] = 1
+        x[index[0]] = -1
+        x[index[1]] = 1
         t = np.random.uniform(0, 1, [self.BATCH_SIZE])
-        #t[index[2]] = 0
-        #t[index[3]] = 1
+        t[index[2]] = 0
+        t[index[3]] = 1
         train_x = x.reshape([self.BATCH_SIZE, 1])
         train_t = t.reshape([self.BATCH_SIZE, 1])
         y = np.sin(np.pi * (train_x * train_x + train_t * train_t))
-        #y = self.test_f(train_x, train_t)
+        # y = self.test_f(train_x, train_t)
         return train_x, train_t, y
 
     def get_valid_data(self):
@@ -133,18 +127,18 @@ class NN:
         x = np.array(x).reshape([self.BATCH_SIZE, 1])
         t = np.array(x).reshape([self.BATCH_SIZE, 1])
         y = np.sin(np.pi * (x * x + t * t))
-        #y = self.test_f(x, t)
+        # y = self.test_f(x, t)
         return x, t, y
 
     def generate_data(self):
         f = open('axis.txt', 'w')
         x = np.random.uniform(-1, 1, [self.BATCH_SIZE])
         index = np.random.randint(0, self.BATCH_SIZE, [4])
-        #x[index[0]] = -1
-        #x[index[1]] = 1
+        # x[index[0]] = -1
+        # x[index[1]] = 1
         t = np.random.uniform(0, 1, [self.BATCH_SIZE])
-        #t[index[2]] = 0
-        #t[index[3]] = 1
+        # t[index[2]] = 0
+        # t[index[3]] = 1
         for i in range(self.BATCH_SIZE):
             f.write(str(x[i]) + ' ' + str(t[i]) + '\n')
         f.close()
@@ -158,15 +152,17 @@ class NN:
 
         global_step = tf.Variable(0, trainable=False)
 
-        #zero = tf.Variable(tf.zeros([self.BATCH_SIZE, 1]))
-        #ones = tf.Variable(tf.ones([self.BATCH_SIZE, 1]))
+        # zero = tf.Variable(tf.zeros([self.BATCH_SIZE, 1]))
+        # ones = tf.Variable(tf.ones([self.BATCH_SIZE, 1]))
         '''loss = tf.reduce_mean(tf.square(self.obj_f(x, t)) +
                               10 * (tf.square(self.func_u(ones, t) - self.boundary_condition(t)) +
                                     tf.square(self.func_u(-ones, t) - self.boundary_condition(t)) +
                                     tf.square(self.func_u(x, zero) - self.initial_condition(x))))'''
 
-        loss = tf.reduce_mean(tf.square(self.test_f(x, t)) + tf.square(y_ - self.func_u(x, t)))
-        loss = tf.reduce_mean(tf.abs(y_-self.func_u(x, t)))
+        #loss = tf.reduce_mean(tf.square(self.test_f(x, t)) + tf.square(y_ - self.func_u(x, t)))
+        #loss = tf.reduce_mean(tf.square(self.test_f(x, t)))
+        loss = tf.reduce_mean(tf.square(self.obj_f(x, t)))
+        # loss = tf.reduce_mean(tf.abs(y_-self.func_u(x, t)))
 
         # 计算L2正则化损失函数
         regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
@@ -185,73 +181,59 @@ class NN:
             TRAINING_STEPS,  # 过完所有训练数据需要的迭代次数
             LEARNING_RATE_DECAY)
 
-        #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+        #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_R, global_step=global_step)
+        #train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss_R, global_step=global_step)
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
-        # 在训练神经网络模型时，每过一遍数据既需要通过反向传播来更新神经网络的参数，
-        # 又要更新每一个参数的滑动平均值。为了一次完成多个操作，TensorFlow提供了两种机制：
-        # tf.control_dependencies()和tf.group()两种机制。
-        # with tf.control_dependencies([train_step,variable_averages_op]):
-        #	train_op = tf.no_op(name='train')
-        # train_op = tf.group(train_step, variable_averages_op)
-
-        # 检验使用了滑动平均模型的神经网络前向传播结果是否正确。
-        # tf.argmax(average_y, 1)计算每个样例的预测答案，
-        # average_y是一个batch_size*10的二维数组，每一行表示一个样例的前向传播结果。
-        # tf.argmax的第二个参数1表示选取最大值的操作仅在第一个维度进行，即只在行中选取最大值对应的下标，
-        # 于是得到一个长度为batch的一维数组，每个样例对应的数字识别结果。
-        # tf.equal判断两个张量的每一维是否相等，相等返回True，否则返回False。
-        # correct_prediction = tf.equal(tf.argmax(average_y, 1), tf.argmax(y_, 1))
-        # 先将布尔型数值转换为实数型，然后计算平均值，即模型在这一组数据上的正确率
-        # accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
-        # accuracy = tf.reduce_mean(tf.square(self.obj_f(x, t)))
         accuracy = tf.reduce_mean(tf.square(self.test_f(x, t)))
 
-        with tf.Session() as sess:
-            init_op = tf.global_variables_initializer()
-            sess.run(init_op)
+        init_op = tf.global_variables_initializer()
+        self.sess.run(init_op)
 
-            # 准备验证数据。
-            self.generate_data()
-            valid_x, valid_t, y = self.get_valid_data()
-            validate_feed = {x: valid_x, t: valid_t, y_: y}
-            validate_acc = sess.run(accuracy, feed_dict=validate_feed)
-            print(validate_acc)
+        # 准备验证数据。
+        self.generate_data()
+        valid_x, valid_t, y = self.get_valid_data()
+        validate_feed = {x: valid_x, t: valid_t, y_: y}
+        validate_acc = self.sess.run(accuracy, feed_dict=validate_feed)
+        print(validate_acc)
 
-            # 迭代训练神经网络
-            for i in range(TRAINING_STEPS):
-                # 产生这一轮使用的一个batch的训练数据，并运行训练过程
-                train_x, train_t, y = self.get_train_data()
-                train_dict = {x: train_x, t: train_t, y_: y}
-                sess.run(train_step, feed_dict=train_dict)
+        # 迭代训练神经网络
+        for i in range(TRAINING_STEPS):
+            # 产生这一轮使用的一个batch的训练数据，并运行训练过程
+            train_x, train_t, y = self.get_train_data()
+            train_dict = {x: train_x, t: train_t, y_: y}
+            self.sess.run(train_step, feed_dict=train_dict)
 
-                # 每100轮输出一次在验证数据集上的测试结果
-                if i % 100 == 0:
-                    validate_acc = sess.run(accuracy, feed_dict=train_dict)
-                    print('After %d training steps, validation accuracy is %g' % (i, validate_acc))
-                    loss_value = sess.run([loss_R, loss], feed_dict=train_dict)
-                    print('loss=', loss_value)
+            # 每100轮输出一次在验证数据集上的测试结果
+            if i % 100 == 0:
+                validate_acc = self.sess.run(accuracy, feed_dict=train_dict)
+                print('After %d training steps, validation accuracy is %g' % (i, validate_acc))
+                loss_value = self.sess.run([loss_R, loss], feed_dict=train_dict)
+                print('loss=', loss_value)
 
-
-            # 在训练结束之后，在测试数据上检测神经网络模型的最终正确率
-            # print(sess.run(self.obj_f(x,t),feed_dict=validate_feed))
-            # print(sess.run(self.func_u(x,t),feed_dict=validate_feed))
-            test_acc = sess.run(accuracy, feed_dict=train_dict)
-            print('After %d training steps, test accuracy using average model is %g' % (TRAINING_STEPS, test_acc))
+        # 在训练结束之后，在测试数据上检测神经网络模型的最终正确率
+        # print(sess.run(self.obj_f(x,t),feed_dict=validate_feed))
+        # print(sess.run(self.func_u(x,t),feed_dict=validate_feed))
+        test_acc = self.sess.run(accuracy, feed_dict=train_dict)
+        print('After %d training steps, test accuracy using average model is %g' % (TRAINING_STEPS, test_acc))
 
     def plotting(self):
         fig = plt.figure()
+        res = 100
         # ax = Axes3D(fig)
         # arange函数用于创建等差数组
-        X = np.arange(-1, 1, 0.02).astype('float32')
-        Y = np.arange(0, 1, 0.01).astype('float32')
+        # X = np.arange(-1, 1, 0.02).astype('float32')
+        # Y = np.arange(0, 1, 0.01).astype('float32')
+        # numpy.linspace函数可以创建一个等差序列的数组，常用3个参数，起始值，终止值，序列长度
+        X = np.linspace(-1, 1, res).astype('float32')
+        Y = np.linspace(0, 1, res).astype('float32')
         # Z = np.random.uniform(0, 1, [100, 100])
-        for i in range(100):
-            x = np.array([X[i]] * 100)
-            x = x.reshape([100, 1])
-            y = Y.reshape([100, 1])
+        for i in range(res):
+            x = np.array([X[i]] * res)
+            x = x.reshape([res, 1])
+            y = Y.reshape([res, 1])
             u = self.sess.run(self.func_u(x, y))
-            f = self.sess.run(self.test_f(x, y))
+            f = self.sess.run(self.obj_f(x, y))
             # print('shape of u:', np.shape(u))
             # u=np.reshape(u,[100,1])
             # print(u)
@@ -294,3 +276,4 @@ def main(argv=None):
 # TensorFlow提供的一个主程序入口，tf.app.run()会调用上面定义的main函数
 if __name__ == '__main__':
     tf.app.run()
+
